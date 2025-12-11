35bf4efecc723d4c5c13522841ed1f7abf2a3563
avijadhav400 <avijadhav400@gmail.com>
2025-12-11 18:36:37 +0530

    Jdbc Task 1

---
+++ src/DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getDBConnection(){
+        Connection con = null;
+        try{
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";
+            String username = "system";
+            String password = "5252";
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if(con!=null){
+            System.out.println("Database Connected Successfully ✅");
+            return con;
+        }else {
+            System.out.println("Database Not Connected ❌");
+            return null;
+        }
+    }
+}
